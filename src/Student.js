fd31dfa358eb5a23031fad9029dff64e0b8a5dff
Akash <srihariakash1998@gmail.com>
2021-07-09 11:53:29 +0530

    updated student component with form

--- src/Student.js
+++ src/Student.js
@@ -20,21 +20,77 @@ function Student(){
              CGPA:"9.0",
             },])
 
-            let dummy={ 
-                studentName:"Ram",
-                 studentDept:"Btech",
-                 RollNumber:"764574",
-                 CGPA:"9.3",
-                }
+        
+                let [boolean,setBoolean] = useState(false);
             const AddStudent=()=>{
-                    let newstudentDetails=[...studentDetails,dummy];
-                    setstudentDetails(newstudentDetails)
+                if(boolean==false)
+                {
+                    setBoolean(true);
+                }
+                else{
+                    setBoolean(false);
+                }
             }
+    let [name,setName]=useState("");
+    let [Dept,setDept]=useState("");
+    let [RollNo,setRollNo]=useState("");
+    let [CGPA,setCGPA]=useState("");
+    const updateName=(e)=>{
+        setName(e.target.value);
+    }   
+    const updateDept=(e)=>{
+        setDept(e.target.value);
+    }   
+    const updateRollno=(e)=>{
+        setRollNo(e.target.value);
+    }   
+    const updateCGPA=(e)=>{
+        setCGPA(e.target.value);
+    }  
+    const submitForm=()=>{
+        let dummy={ 
+            studentName:name,
+             studentDept:Dept,
+             RollNumber:RollNo,
+             CGPA:CGPA,
+            }
+            let newStudentDetails = [...studentDetails,dummy];
+            setstudentDetails(newStudentDetails);
+            setName("");
+            setDept("");
+            setRollNo("");
+            setCGPA("");
+            setBoolean(false);
+        
+    }         
     return(
         <div>
+            {boolean==true &&(<form>
+        <h1>Add New Student Details</h1>
+        <p>Enter Student Name:</p>
+        <input
+          type="text" onChange={updateName}
+        />
+        <p>Enter Student Dept:</p>
+        <input
+          type="text" onChange={updateDept}
+        />
+        <p>Enter Student RollNumber:</p>
+        <input
+          type="text" onChange={updateRollno}
+        />
+        <p>Enter CGPA:</p>
+        <input
+          type="text" onChange={updateCGPA}
+        />
+        <button onClick={submitForm}>
+            Submit
+        </button>
+      </form>)}
+        
             <Semester student= {studentDetails}/>
             <button onClick={AddStudent}>
-                Add 
+                Add Student
             </button>
         </div>
     )
